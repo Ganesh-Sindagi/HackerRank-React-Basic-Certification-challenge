b46c0a623e1f73dd2893fb616debe6f903141c1e
Ganesh <ganeshsindagi7@gmail.com>
2021-01-14 12:46:45 +0530

    after the detils

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,37 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Slides({slides}) {
     const [count, setCount] = useState(0);
 
     function handleRestart() {
         setCount(0);
-        document.getElementById("button-restart").disabled = true;
+        document.getElementById("res-btn").disabled = true;
         document.getElementById("prev-btn").disabled = true;
         document.getElementById("next-btn").disabled = false;
     }
-    
+
     function handlePrev() {
-        if(count===0) {
+        document.getElementById("next-btn").disabled = false; 
+        if(count===1) {
+            document.getElementById("res-btn").disabled = true;
             document.getElementById("prev-btn").disabled = true;
-            document.getElementById("next-btn").disabled = false; 
+            document.getElementById("next-btn").disabled = false;
+            setCount(0) 
         } else {
             setCount(count - 1); 
         } 
     }
 
     function handleNext() {
+        document.getElementById("res-btn").disabled = false;
         document.getElementById("prev-btn").disabled = false; 
         if(count===3) {
-            document.getElementById("next-btn").disabled = true; 
+            document.getElementById("next-btn").disabled = true;
+            setCount(4) 
         } else {
             setCount(count + 1);
         }
     }
 
+    useEffect(()=>{
+        document.getElementById("res-btn").disabled = true;
+        document.getElementById("prev-btn").disabled = true;
+    }, [])
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined" onClick={handleRestart}>Restart</button>
+                <button id="res-btn" data-testid="button-restart" className="small outlined" onClick={handleRestart}>Restart</button>
                 <button id="prev-btn" data-testid="button-prev" className="small" onClick={handlePrev}>Prev</button>
                 <button id="next-btn" data-testid="button-next" className="small" onClick={handleNext}>Next</button>
             </div>
