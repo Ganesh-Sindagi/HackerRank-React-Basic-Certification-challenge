c3e452bcff4b873e8f61471e2e1e3cb229145999
Ganesh <ganeshsindagi7@gmail.com>
2021-01-14 12:17:54 +0530

    After Test

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Slides({slides}) {
+    console.log(slides);
+
+    const [count, setCount] = useState(0);
+
+    function handleRestart() {
+        setCount(0);
+        document.getElementById("prev-btn").disabled = true;
+        document.getElementById("next-btn").disabled = false;
+    }
+    
+    function handlePrev() {
+        if(count===0) {
+            document.getElementById("prev-btn").disabled = true;
+            document.getElementById("next-btn").disabled = false; 
+        } else {
+            setCount(count - 1); 
+        } 
+    }
+
+    function handleNext() {
+        document.getElementById("prev-btn").disabled = false; 
+        if(count===3) {
+            document.getElementById("next-btn").disabled = true; 
+        } else {
+            setCount(count + 1);
+        }
+    }
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={handleRestart}>Restart</button>
+                <button id="prev-btn" data-testid="button-prev" className="small" onClick={handlePrev}>Prev</button>
+                <button id="next-btn" data-testid="button-next" className="small" onClick={handleNext}>Next</button>
             </div>
+
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[count].title}</h1>
+                <p data-testid="text">{slides[count].text}</p>
             </div>
         </div>
     );
